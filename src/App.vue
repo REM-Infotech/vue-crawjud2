eb41d4d11a82390730b0fb748cb977a0938a39f1
Robotz213 <nicholas@robotz.dev>
2025-05-18 09:08:44 -0400

    feat: add vue-router for routing support and update dependencies

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NavbarView from "@/components/NavbarView.vue";
+import { RouterView } from "vue-router";
+import ChoseThemeView from "./components/ChoseThemeView.vue";
+</script>
+
+<template>
+  <NavbarView />
+  <RouterView />
+  <ChoseThemeView />
+</template>
