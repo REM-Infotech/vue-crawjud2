fccf978cb7144a71113457492c8be1ed5da1a0eb
Robotz213 <nicholas@robotz.dev>
2025-05-22 16:36:23 -0300

    refactor: remove BootstrapVueNext and unplugin-vue-components from Vite configuration files

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,16 +1,9 @@
 import vue from "@vitejs/plugin-vue";
-import { BootstrapVueNextResolver } from "bootstrap-vue-next";
-import Components from "unplugin-vue-components/vite";
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [BootstrapVueNextResolver()],
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
     alias: {
@@ -18,6 +11,6 @@ export default defineConfig({
     },
   },
   build: {
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 });
